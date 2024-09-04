4d15b39b36cc0fd1022434afb413ea649923ff69
jjuya <404_jjuya@kakao.com>
2024-09-04 10:03:27 +0900

    feat(i18n) : edit greeting content

--- src/components/landing/HomeAboutUsSection.tsx
+++ src/components/landing/HomeAboutUsSection.tsx
@@ -3,7 +3,12 @@ import { useTranslations } from 'next-intl'
 import { Col, Row } from 'antd'
 import { IntroSection } from './home.style'
 
-export const HomeAboutUsSection = ({ CDN_ENDPOINT, ...props }: { CDN_ENDPOINT: string | undefined }) => {
+export const HomeAboutUsSection = ({
+	CDN_ENDPOINT,
+	...props
+}: {
+	CDN_ENDPOINT: string | undefined
+}) => {
 	const t = useTranslations('ABOUT_US')
 
 	return (
@@ -25,45 +30,7 @@ export const HomeAboutUsSection = ({ CDN_ENDPOINT, ...props }: { CDN_ENDPOINT: s
 						xs={24}
 						className="intro-description-wrapper">
 						<h3 className="intro-title">{t('greeting.subtitle')}</h3>
-						<p className="intro-description">
-							I am very happy to introduce you to our BOB Agricultural Products Smart
-							Processing Plant.
-						</p>
-						<p className="intro-description">
-							BOB is a smart agricultural product processing company that focuses on
-							seeding, cultivating, harvesting, distributing, processing, and
-							exporting high-quality agricultural products based on cutting-edge
-							technology and expertise.
-						</p>
-						<p className="intro-description">
-							Our BOB plant is equipped with the latest technology and equipment, and
-							agricultural experts work together to produce the best quality and best
-							results, and we grow and harvest crops such as various vegetables and
-							fruits to provide fresh and healthy agricultural products.
-						</p>
-
-						<p className="intro-description">
-							Bob's goal is to provide the safest and highest quality agricultural
-							products to customers around the world.
-						</p>
-
-						<p className="intro-description">
-							Bob Co., Ltd. operates a strict quality control system to ensure that
-							all products are safe and fresh, pursues sustainable agriculture, and
-							emphasizes environmental protection and social responsibility. If we
-							cooperate with BOB Co., Ltd., we can stably supply high-quality
-							agricultural products through smart farms and smart factory management
-							systems, and we can build a successful business together through our
-							expertise and experience.
-						</p>
-
-						<p className="intro-description">
-							If you would like more information or are interested in cooperation,
-							please feel free to contact us. Bob Co., Ltd. will respond promptly and
-							kindly to your inquiries at any time.
-						</p>
-
-						<p className="intro-description">Thank you very much.</p>
+						<div dangerouslySetInnerHTML={{__html: t.raw('greeting.content')}}/>
 
 						<div className="sign-wrapper">
 							<p>
